c49d3b3e622bb39004c85212593084e324260035
Shruthi Vikram <vikr0006@algonquinlive.com>
2020-01-13 15:54:47 -0500

    Added index, readme and script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = 'Shruthi Vikram';
+console.log(`Hello, my name is ${name}`);
+
+let number = 5
+let result = 0
+// console.log(`${number} * 2 =  ${result = number * 2}`)
+// console.log(`${result} * 5 =  ${result = result * 5}`)
+// console.log(`${result} / ${number} = ${result / number})
