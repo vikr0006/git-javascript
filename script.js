21758011fee5fbf3a488438dd41fe43389df233a
Shruthi Vikram <vikr0006@algonquinlive.com>
2020-01-13 16:05:01 -0500

    Added equations

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log(`Hello, my name is ${name}`);
 
 let number = 5
 let result = 0
-// console.log(`${number} * 2 =  ${result = number * 2}`)
-// console.log(`${result} * 5 =  ${result = result * 5}`)
-// console.log(`${result} / ${number} = ${result / number})
+console.log(`${number} * 2 =  ${result = number * 2}`)
+console.log(`${result} * 5 =  ${result = result * 5}`)
+console.log(`${result}/${number} = ${result = result / number}`)
+console.log(`${result} - 7 = ${result = result - 7}`)
